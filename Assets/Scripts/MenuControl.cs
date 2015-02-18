2bb60b5a3e3587a5929672f93b935f25c7bc55c3
pililopez <pililoji@hotmail.com>
2015-02-18 18:42:02 +0100

    No consigo gran cosa
    animación de panel de instrucciones e intento de que funcione el script
    del menú

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -2,34 +2,26 @@
 using System.Collections;
 
 public class MenuControl : MonoBehaviour {
-	/*private int bWidth = 3;
-	private int bHeigth = 1;
-	private float posx = (Screen.width / 2)-150.1f;
-	private float posy = (Screen.height / 2)48f;
 
-	public GUISkin skin;*/
-	// Use this for initialization
 
-	public class Button : Button Jugar ;
+	public void  Instrucciones(){
 
-	void Start () {
-	
+		animation.Instrucciones.true};
+	    if (Input.GetMouseButtonDown ("0")){
+		else {animation.Instrucciones.false}
 	}
-	
-	// Update is called once per frame
 
-
-	void Update () {
-		if (Input.GetMouseButtonDown(0) Button Jugar "Button Jugar") {
-			Application.LoadLevel("fase1");
-		}
+	public void Salir(){
+		Application.Quit ();
 	}
-	/*void OnGUI () {
-		GUI.skin = skin;
-		
-		if (GUI.Button (new Rect (posx, posy, bHeigth, bWidth), "Button Jugar")) {
-			Application.LoadLevel ("fase 1");
-		}
-	}*/
 
+	public void Jugar(){
+		Application.LoadLevel ("fase1");
+	}
+
+	public void Logros(){
+		if (Input.GetMouseButtonDown ("0")) {
+				
+		}
+		}
 	}
