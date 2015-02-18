f5b13df6ea4ebc0950f48942dace7cb645cc957e
pililopez <piloloji@hotmail.com>
2015-02-18 13:34:51 +0100

    350# Script menu
    10%, no hay manera

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+	/*private int bWidth = 3;
+	private int bHeigth = 1;
+	private float posx = (Screen.width / 2)-150.1f;
+	private float posy = (Screen.height / 2)48f;
+
+	public GUISkin skin;*/
+	// Use this for initialization
+
+	public class Button : Button Jugar ;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0) Button Jugar "Button Jugar") {
+			Application.LoadLevel("fase1");
+		}
+	}
+	/*void OnGUI () {
+		GUI.skin = skin;
+		
+		if (GUI.Button (new Rect (posx, posy, bHeigth, bWidth), "Button Jugar")) {
+			Application.LoadLevel ("fase 1");
+		}
+	}*/
+
+	}
