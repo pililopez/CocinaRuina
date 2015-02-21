8a8ac0667c8acbbd7b55c27aff1d1d8c78e2812d
pililopez <piloloji@hotmail.com>
2015-02-21 20:11:58 +0100

    370# y 391# 100%
    Script aparición enemigos y borrada por doble respectivamente

---
+++ Assets/Scripts/AparicionEnemigos.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AparicionEnemigos : MonoBehaviour {
+
+	public float spawnTime=1f;
+	public float spawnDelay=1f;
+	public GameObject[] enemies;
+	public int maxEnemies=4;
+	
+	private GameObject[] enemiesCreated;
+	// Use this for initialization
+	void Start () {
+	InvokeRepeating ("DispensEnemigos", spawnDelay, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+	void CreaEnemigo (){
+		int enemyIndex=Random.Range(0, enemies.Length);
+		enemiesCreated = GameObject.FindGameObjectsWithTag("Enemigo");
+		if (enemiesCreated.Length < maxEnemies){
+		Instantiate (enemies [enemyIndex], transform.position, transform.rotation);
+			}
+			
+		}
+	
+}
