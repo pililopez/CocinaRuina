3f7aab2cd723cc3d0189b0e31ee4ceff2e0c850e
pililopez <piloloji@hotmail.com>
2015-02-24 14:29:47 +0100

    339# y 367#
    70% y 50% respectivamente, ambos dependen de otros.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveForce = 365f;            
+	public float maxSpeed = 5f; 
+
+//	private LiveController live;
+	private ParticleSystem Sangre;
+	// Use this for initialization
+	void Start () {
+//		live = GameObject.Find ("Live").GetComponent<LiveController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate (){
+	// Cache the horizontal input.
+		float h = Input.GetAxis ("Horizontal");
+		
+		
+		if (h * rigidbody2D.velocity.x < maxSpeed)             
+			rigidbody2D.AddForce (Vector2.right * h * moveForce);                  
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		float v = Input.GetAxis ("Vertical");
+		
+		
+		if (v * rigidbody2D.velocity.y < maxSpeed)             
+			rigidbody2D.AddForce (Vector2.up * v * moveForce);                  
+		if (Mathf.Abs (rigidbody2D.velocity.y) > maxSpeed)
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.y) * maxSpeed, rigidbody2D.velocity.y);
+
+
+
+		}
+
+/*	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Enemigo"){
+			live.total_live -=10;
+			Destroy(col.gameObject);
+		}
+
+		if (col.gameObject.tag == "Ingrediente"){
+			live.total_live +=20;
+			Destroy(col.gameObject);
+		}
+	}*/
+
+}
---
+++ Assets/Scripts/VidaPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VidaPlayer : MonoBehaviour {
+
+	public int total_live = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		guiText.text = "Vida: "+ total_live;
+		if (total_live <= 0) {
+			Application.LoadLevel ("Menuinicio");
+		}
+		
+	}
+	
+}
+
