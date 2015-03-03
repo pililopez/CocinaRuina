4c70282976dd23c314552a45e3d5b5b8538e4e06
pililopez <piloloji@hotmail.com>
2015-03-03 20:18:00 +0100

    Me crecen los enanos del circo

--- Assets/Scripts/ControlEnemigos.cs
+++ Assets/Scripts/ControlEnemigos.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class ControlEnemigos : MonoBehaviour {
-	public Vector2 speed = new Vector2(10, 10);
-	public Vector2 direction = new Vector2(-1, 0);
+	//public Vector2 speed = new Vector2(10, 10);
+	//public Vector2 direction = new Vector2(-1, 0);
 	public AudioClip aparece;
-	public float amplitudeY=1.0f;
-	public float omegaY=1.0f;
+	//public float amplitudeY=1.0f;
+	//public float omegaY=1.0f;
 	public int puntos = 1;
 	public float velocidad = 5;
-	public GameObject golpe;
-	private float goRight = -1f;
-	private float goUp = -1f;
+	//public GameObject golpe;
+	private float goRight = 1f;
+//	private float goUp = -1f;
 	//private bool destruido = false;	
 	private float index;
 	private Vector2 movement;
@@ -19,12 +19,13 @@ public class ControlEnemigos : MonoBehaviour {
 	private ParticleSystem particle;
 	private GameControl total_puntos;
 	public bool destroyed = false;
+	private Vector3 rotacion;
 
 	// Use this for initialization
 	void Start () {
 		AudioSource.PlayClipAtPoint(aparece, transform.position);
 		var daño = transform.Find ("Parti_limon");
-		particle = (ParticleSystem)daño.GetComponent ("Parti_limon");
+		//particle = (ParticleSystem)daño.GetComponent ("Parti_limon");
 		particle.enableEmission = false;
 	}
 	
@@ -49,18 +50,24 @@ public class ControlEnemigos : MonoBehaviour {
 	void FixedUpdate(){
 		if (!destroyed) {
 			rigidbody2D.velocity = movement;
-			Vector2 v2 = new Vector2 (goRight * velocidad, goUp * velocidad);
+			Vector2 v2 = new Vector2 (0, goRight * velocidad);
 			rigidbody2D.velocity = v2;
+			//Vector3 localy = new Vector3 (0,0,transform.rotation.z ( 2.5f));
 				} 
 		}
 	void OnCollisionEnter2D(Collision2D col){
 
 	if (col.gameObject.tag == "Margen") {
-		Vector3 localx = new Vector3(transform.localScale.x * -1,transform.localScale.y,transform.localScale.z );
+		Vector3 localx = new Vector3(transform.localScale.x ,transform.localScale.y * -1,transform.position.z );
 		transform.localScale = localx ;
 		goRight *= -1;
-		goUp *= -1;
+		Vector3 = new Vector3(transform.rotation.x ,transform.rotation.y ,transform.rotation.z (25));
 		}
-
+	if (col.gameObject.tag == "Enemigo") {
+		Vector3 localx = new Vector3(transform.localScale.x ,transform.localScale.y * -1,transform.position.z );
+		transform.localScale = localx ;
+		goRight *= -1;
+			
+		}
 	}
 }
---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparo : MonoBehaviour {
+
+	public GameObject lemon;
+	public GameObject puntoDisparo;
+	public float velocidad = 5f;
+	GameObject nueva_bala;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.B)) {
+			nueva_bala = (GameObject)Instantiate(lemon,puntoDisparo.transform.position,Quaternion.identity);
+			nueva_bala.rigidbody.AddForce(puntoDisparo.transform.forward*velocidad);}
+		Destroy (nueva_bala, 2);
+		
+	}
+
+}
